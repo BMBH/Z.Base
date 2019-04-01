7d546769ff7813280f3c9275bf464143f90f9654
zhangyp <670633706@qq.com>
2019-04-01 14:49:08 +0800

    Dll Generate

--- test/Z.Base.Test.Log/LogFormatTests.cs
+++ test/Z.Base.Test.Log/LogFormatTests.cs
@@ -38,36 +38,6 @@ namespace Z.Base.Test.Log
         }
         #endregion
 
-        #region 附加测试特性
-        // 
-        //编写测试时，还可使用以下特性:
-        //
-        //使用 ClassInitialize 在运行类中的第一个测试前先运行代码
-        //[ClassInitialize()]
-        //public static void MyClassInitialize(TestContext testContext)
-        //{
-        //}
-        //
-        //使用 ClassCleanup 在运行完类中的所有测试后再运行代码
-        //[ClassCleanup()]
-        //public static void MyClassCleanup()
-        //{
-        //}
-        //
-        //使用 TestInitialize 在运行每个测试前先运行代码
-        //[TestInitialize()]
-        //public void MyTestInitialize()
-        //{
-        //}
-        //
-        //使用 TestCleanup 在运行完每个测试后运行代码
-        //[TestCleanup()]
-        //public void MyTestCleanup()
-        //{
-        //}
-        //
-        #endregion
-
         #region CommStr 的测试
         /// <summary>
         ///CommStr 的测试
--- test/Z.Base.Test.Log/LoggerTests.cs
+++ test/Z.Base.Test.Log/LoggerTests.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Z.Base.Log;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Z.Base.Test.Log
+namespace Z.Base.Test.Utils
 {
-    class LoggerTests
+    [TestClass()]
+    public class LoggerTests
     {
+        [TestMethod()]
+        public void AllTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DebugTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InfoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void WarnTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ErrorTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FatalTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void NoneTest()
+        {
+            Assert.Fail();
+        }
     }
 }

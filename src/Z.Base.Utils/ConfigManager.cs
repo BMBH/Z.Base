3a6bf52e959fc927dc7e882bd52681e58bd54e63
zhangyp <670633706@qq.com>
2020-04-02 12:02:53 +0800

    ConfigManager重构

--- src/Z.Base.Utils/ConfigManager.cs
+++ src/Z.Base.Utils/ConfigManager.cs
@@ -23,19 +23,19 @@ namespace Z.Base.Utils
         /// 按键获取应用程序设置的文本
         /// </summary>
         /// <param name="key">关键字</param>
-        /// <param name="config">config配置文件，默认null</param>
+        /// <param name="configName">config配置文件，默认null</param>
         /// <returns>文本</returns>
-        public static string StringVal(string key, string config = null)
+        public static string StringVal(string key, string configName = null)
         {
             if (string.IsNullOrEmpty(key))
                 return string.Empty;
-            if (string.IsNullOrEmpty(config))//提供为空的情况
-                return ConfigurationManager.AppSettings[key];
 
-            Configuration configuration = ConfigManager.Configuration(config);
-            if (configuration == null)
+            Configuration configuration = Configuration(configName);
+            if (configuration == null || configuration.AppSettings.Settings.Count <= 0)
                 return string.Empty;
-            return configuration.AppSettings.Settings[key].Value;
+            if (configuration.AppSettings.Settings.AllKeys.Contains(key))
+                return configuration.AppSettings.Settings[key].Value;
+            return string.Empty;
         }
         #endregion
 
@@ -44,16 +44,16 @@ namespace Z.Base.Utils
         /// 按键获取应用程序设置的布尔值
         /// </summary>
         /// <param name="key">关键字</param>
-        /// <param name="config">config配置文件，默认null</param>
+        /// <param name="configName">config配置文件，默认null</param>
         /// <returns>布尔值</returns>
-        public static bool BoolVal(string key, string config = null)
+        public static bool BoolVal(string key, string configName = null)
         {
-            string val = StringVal(key, config);
+            string val = StringVal(key, configName);
             if (string.IsNullOrEmpty(val))
                 return false;
 
             bool temp = false;
-            bool result = bool.TryParse(val,out temp);
+            bool result = bool.TryParse(val, out temp);
             if (result)
                 return temp;
 
@@ -66,20 +66,20 @@ namespace Z.Base.Utils
         /// 按键获取应用程序设置的整数
         /// </summary>
         /// <param name="key">关键字</param>
-        /// <param name="config">config配置文件，默认null</param>
+        /// <param name="configName">config配置文件，默认null</param>
         /// <returns>整数</returns>
-        public static int IntVal(string key, string config = null)
+        public static int IntVal(string key, string configName = null)
         {
-            string val = StringVal(key, config);
+            string val = StringVal(key, configName);
             if (string.IsNullOrEmpty(val))
-                return 0;
+                return -1;
 
-            int temp = 0;
+            int temp = -1;
             bool result = int.TryParse(val, out temp);
             if (result)
                 return temp;
 
-            return 0;
+            return -1;
         }
         #endregion
 
@@ -88,20 +88,64 @@ namespace Z.Base.Utils
         /// 按键获取应用程序设置的单精度浮点数值
         /// </summary>
         /// <param name="key">关键字</param>
-        /// <param name="config">config配置文件，默认null</param>
+        /// <param name="configName">config配置文件，默认null</param>
         /// <returns>单精度浮点数值</returns>
-        public static float FloatVal(string key, string config = null)
+        public static float FloatVal(string key, string configName = null)
         {
-            string val = StringVal(key, config);
+            string val = StringVal(key, configName);
             if (string.IsNullOrEmpty(val))
-                return 0f;
+                return -1f;
 
-            float temp = 0f;
+            float temp = -1f;
             bool result = float.TryParse(val, out temp);
             if (result)
                 return temp;
 
-            return 0f;
+            return -1f;
+        }
+        #endregion
+
+        #region 按键移除应用程序设置
+        /// <summary>
+        /// 按键移除应用程序设置
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="configName"></param>
+        public static bool RemoveKey(string key, string configName = null)
+        {
+            if (key.IsEmpty())
+                return false;
+
+            Configuration configuration = Configuration(configName);
+            if (configuration == null || configuration.AppSettings.Settings.Count <= 0)
+                return false;
+
+            if (configuration.AppSettings.Settings.AllKeys.Contains(key))
+            {
+                configuration.AppSettings.Settings.Remove(key);
+                configuration.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection("appSettings");
+                return true;
+            }
+            return false;
+        }
+        #endregion
+
+        #region 按键值设置应用程序配置
+        /// <summary>
+        /// 按键值设置应用程序配置
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="configName"></param>
+        public static void SetValue(string key, string value, string configName = null)
+        {
+            if (key.IsEmpty() || value.IsEmpty()) return;
+
+            Configuration configuration = Configuration(configName);
+            if (configuration == null || configuration.AppSettings.Settings.Count <= 0)
+                return;
+            PerformSet(key, value, configuration);
         }
         #endregion
 
@@ -113,20 +157,43 @@ namespace Z.Base.Utils
         /// <summary>
         /// 按 fileName 和 dir 生成适用于特定计算机、应用程序或资源的配置文件操作的对象
         /// </summary>
-        /// <param name="fileName">文件名称</param>
+        /// <param name="configName">文件名称</param>
         /// <param name="dir">路径，默认""</param>
         /// <returns>表示适用于特定计算机、应用程序或资源的配置文件的对象实例</returns>
-        private static Configuration Configuration(string fileName, string dir = "")
+        private static Configuration Configuration(string configName, string dir = "")
         {
-            if (string.IsNullOrEmpty(fileName) || dir == null)
-                return null;
-            string path = Path.Combine(FileManager.BaseDirectory(), dir);
+            if (configName.IsEmpty() || dir == null)//路径不满足条件则返回默认App.Config
+                return ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
 
-            ExeConfigurationFileMap configFileMap = new ExeConfigurationFileMap();
-            configFileMap.ExeConfigFilename = Path.Combine(path, fileName);
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, dir);//组合路径
+
+            ExeConfigurationFileMap configFileMap = new ExeConfigurationFileMap();//组合对象
+            configFileMap.ExeConfigFilename = Path.Combine(path, configName);
 
             Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configFileMap, ConfigurationUserLevel.None);
             return config;
+        }
+        #endregion
+
+        #region 执行应用程序配置设置
+        /// <summary>
+        /// 执行应用程序配置设置
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="configuration"></param>
+        private static void PerformSet(string key, string value, Configuration configuration)
+        {
+            if (configuration.AppSettings.Settings.AllKeys.Contains(key))
+            {
+                configuration.AppSettings.Settings[key].Value = value;
+            }
+            else
+            {
+                configuration.AppSettings.Settings.Add(key, value);
+            }
+            configuration.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
         }
         #endregion
 
